34e66059dc62ed7c01e5a28735fea174180037fa
actytauri <bagasrakha.9h@gmail.com>
2023-06-14 17:16:01 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,110 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid d-flex justify-content-center align-items-center" style="height:100vh; overflow:hidden;">
+    <div class="row main">
+      <div class="col-lg p-3 divide">
+        <div style="width: 20%;"></div>
+        <div style="width: 80%;">
+          <div class="fw-bold text-white text-start">Jakarta</div>
+          <div class="d-flex position-relative status-cuaca">
+            <img class="berawan" alt="berawan" src="/img/berawan.png">
+            <div class="status text-white px-4 ps-5 fs-1">Berawan , 31° C</div>
+          </div>
+          <div class="line"></div>
+          <div class="text-white text-start px-2 fs-1 fw-semibold">Keep Safe</div>
+        </div>
+      </div>
+
+      <div class="col-lg divides">
+        <div class="d-flex justify-content-center align-items-center" style="width: 100%; height: 45px;">
+          <div class="tgl text-white">Kamis, 8 November 2021</div>
+          <div class="line-tgl"></div>
+        </div>
+        <div class="d-flex justify-content-center card-container flex-row">
+          <CardWeather :cuaca="cuaca"/>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardWeather from './components/CardWeather.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    components: { CardWeather },
+    data() {
+      return {
+        cuaca: [
+          {
+            kota: 'Yogyakarta',
+            status: 'cerah',
+            suhu: '32° C'
+          },
+          {
+            kota: 'Bekasi',
+            status: 'hujan',
+            suhu: '32° C'
+          },
+        ]
+      }
+    },
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Roboto', sans-serif;
+  height: 100vh;
+  background: linear-gradient(130.54deg, #27A5F9 26.95%, rgba(39, 165, 249, 0) 123.41%);
+}
+.main {
+  width: 100%;
+  padding: 0 60px;
+}
+.fw-bold {
+  font-size: 110px;
+}
+.status-cuaca {
+  margin-top: -25px;
+}
+.status {
+  background: rgba(238, 238, 238, 0.5);
+  border-radius: 10px;
+  margin-left: 40px;
+}
+.line {
+  background-color: white;
+  width: 95%;
+  height: 3px;
+  margin-top: 40px;
+}
+.berawan {
+  position: absolute;
+  height: 60px;
+}
+.divide {
+  display: flex;
+  width: 50%;
+}
+.divides {
+  display: flex;
+  width: 50%;
+  flex-direction: column;
+}
+.tgl {
+  font-size: 27px;
+  font-weight: 300;
+  width: 50%;
+}
+.line-tgl {
+  background-color: white;
+  height: 3px;
+  width: 50%;
+}
+.card-container {
+  gap: 10%;
+  margin-top: 10px;
 }
 </style>
---
+++ src/components/CardWeather.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-for="(data, index) in cuaca" :key="index">
+    <div class="card d-flex justify-content-center align-items-center flex-column">
+      <div class="item fs-2 fw-light">{{ data.kota }}</div>
+      <div class="item image"><img :src="'../img/' + data.status + '.png'" :alt="data.status"></div>
+      <div class="item fs-1 fw-bold">{{ data.suhu }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardWeather',
+  props: {
+    cuaca: Array
+  },
+  methods: {
+  getWeatherImage(cuaca) {
+    // Mengembalikan path gambar sesuai dengan cuaca
+    return `<img src="../assets/${cuaca}.png">`;
+  }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  width: 300px;
+  border-radius: 20px;
+  height: 400px;
+}
+.item {
+  width: 100%;
+  height: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.item img {
+  width: 40%;
+}
+.image {
+  height: 40%;
+}
+</style>
